caa56ebe64ea66592c90471a9a1c295dadab6c2c
MMoudy49 <mmoudy49@gmail.com>
2020-02-10 14:02:42 -0600

    Testing Prettier commit hook formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = () => {
   return (
@@ -18,9 +18,10 @@ const App = () => {
         >
           Learn React
         </a>
+        <p>testing prettier commit hook formatter</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
